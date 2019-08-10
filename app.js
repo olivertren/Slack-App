ee07f72e56b8da6028c5828aa3e5eee561074e2e
Connor Barley <connorwbarley@gmail.com>
2019-08-10 10:06:16 -0400

    fix

--- app.js
+++ app.js
@@ -16,32 +16,45 @@ const headers = {
 ////////////
 
 app.get("/", (req, res) => {
-  res.send("Klaviyo NYT Mini App")
-})
+  res.send("Klaviyo NYT Mini App");
+});
 
 app.post("/", (req, res) => {
-  getMessages(ROOT_URL).then(async data => {
+  getMessages(ROOT_URL)
+    .then(async data => {
       const sortedArray = await mapAndSort(data);
-      const grouped = groupBy(sortedArray, "day_of_year").filter(item => item !== undefined)
+      const grouped = groupBy(sortedArray, "day_of_year").filter(
+        item => item !== undefined
+      );
       const pointsArray = assignPoints(grouped);
       const nowFlatten = [].concat(...pointsArray);
       const combinedScores = nowFlatten.reduce((acc, elem) => {
         if (acc.filter(el => el.id === elem.id)[0]) {
-          acc.filter((el) => el.id == elem.id)[0].points += elem.points;
+          acc.filter(el => el.id == elem.id)[0].points += elem.points;
         } else acc.push(elem);
-        return acc
-      }, [])
-      return combinedScores
+        return acc;
+      }, []);
+      return combinedScores;
     })
     .then(response => {
-      let newRes = response.sort((a, b) => (a.points < b.points ? 1 : -1)).map((person, index) => `${index + 1}) *USER*: @${person.slack_handle} || *SCORE*: ${person.points}\n`).join("");
-      let sendToSlack = "*==========WEEKLY LEADERBOARD==========*\n_5pts for 1st place - 1pt for 5th. Zero for rest_ \n\n" + newRes;
-      res.status(200).send(sendToSlack)
+      let newRes = response
+        .sort((a, b) => (a.points < b.points ? 1 : -1))
+        .map(
+          (person, index) =>
+            `${index + 1}) *USER*: @${person.slack_handle} || *SCORE*: ${
+              person.points
+            }\n`
+        )
+        .join("");
+      let sendToSlack =
+        "*==========WEEKLY LEADERBOARD==========*\n_5pts for 1st place - 1pt for 5th. Zero for rest_ \n\n" +
+        newRes;
+      res.status(200).send(sendToSlack);
     })
     .catch(err => {
-      throw new Error(err)
+      throw new Error(err);
     });
-})
+});
 
 ///////////////////
 // API FUNCTIONS //
@@ -51,7 +64,8 @@ app.post("/", (req, res) => {
 const getMessages = url => {
   return axios
     .get(
-      `${url}${messagesEndpoint}`, {
+      `${url}${messagesEndpoint}`,
+      {
         params: {
           token: process.env.USER_TOKEN,
           channel: "CLB4S6GNS",
@@ -63,16 +77,13 @@ const getMessages = url => {
       headers
     )
     .then(res => {
-      const {
-        messages
-      } = res.data;
-      console.log(messages)
+      const { messages } = res.data;
       return messages.filter(
         item =>
-        item.type === "message" &&
-        !item.subtype &&
-        (convertSecondsToDay(item.ts) !== 0 &&
-          convertSecondsToDay(item.ts) !== 6)
+          item.type === "message" &&
+          !item.subtype &&
+          (convertSecondsToDay(item.ts) !== 0 &&
+            convertSecondsToDay(item.ts) !== 6)
       );
     })
     .catch(err => {
@@ -88,10 +99,7 @@ const getUserById = id => {
       headers
     )
     .then(res => {
-      const {
-        real_name,
-        name
-      } = res.data.user;
+      const { real_name, name } = res.data.user;
       return {
         realName: real_name,
         username: name
@@ -113,8 +121,10 @@ const convertSecondsToDay = seconds => {
 const getMonday = () => {
   let prevMonday = new Date();
   prevMonday.setHours(0, 0, 0, 0);
-  let mondayMidnight = prevMonday.setDate(prevMonday.getDate() - (prevMonday.getDay() + 6) % 7);
-  return mondayMidnight / 1000
+  let mondayMidnight = prevMonday.setDate(
+    prevMonday.getDate() - ((prevMonday.getDay() + 6) % 7)
+  );
+  return mondayMidnight / 1000;
 };
 
 const getDayofYear = inp => {
@@ -136,15 +146,22 @@ const mapAndSort = async data => {
     return {
       id: item.user,
       slack_handle: response.username,
-      time: convertTextToNum(item.text).toString().length > 3 ? parseInt(convertTextToNum(item.text).toString().slice(0, -1)) : convertTextToNum(item.text),
+      time:
+        convertTextToNum(item.text).toString().length > 3
+          ? parseInt(
+              convertTextToNum(item.text)
+                .toString()
+                .slice(0, -1)
+            )
+          : convertTextToNum(item.text),
       day_of_year: getDayofYear(item.ts)
-    }
+    };
   });
   const response = await Promise.all(promises);
   let filtered = response
     .filter(item => item.time !== "")
     .sort((a, b) => (a.day_of_year > b.day_of_year ? 1 : -1));
-  return filtered
+  return filtered;
 };
 
 const convertTextToNum = input =>
@@ -158,21 +175,21 @@ const groupBy = (arr, property) => {
     memo[x[property]].push(x);
     return memo;
   }, []);
-}
+};
 
 const assignPoints = data => {
   return data.map(item => {
     let newItem = item.sort((a, b) => (a.time > b.time ? 1 : -1));
     let num = 5 || newItem.length;
-    const topFive = newItem.slice(0, num)
+    const topFive = newItem.slice(0, num);
     const newTopFive = topFive
       .map((i, index) => ({
         ...i,
         points: 5 - index
       }))
       .reverse();
-    return newTopFive
-  })
-}
+    return newTopFive;
+  });
+};
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
